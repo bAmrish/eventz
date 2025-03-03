687a43670f539c5ca36181ac19ea7cec761f53e6
Amrish Bharatiya <bamrish@gmail.com>
2025-03-03 09:09:25 +0530

    Updated index listing for event to only show upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ApplicationRecord
+  
+  # Get all the upcoming events 
+  # ordered in ascending order of when they are starting
+  def self.upcoming
+    where("starts_at > ?", Time.now).order("starts_at")  
+  end
+
   def free?()
     return price.blank? || price.zero?
   end
