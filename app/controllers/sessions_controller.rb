80d2d9d8469735d0844e9907f2bd40b9fbd54a6c
Amrish Bharatiya <bamrish@gmail.com>
2025-03-13 12:10:28 +0530

    Added intial session code with sign up page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    email = params[:email]
+    password = params[:password]
+  end
+
+  def destroy
+    
+  end
+end
