3a4c582a40e67254f87420bfc66ae1e5fc9f02fb
Amrish Bharatiya <bamrish@gmail.com>
2025-03-13 12:25:33 +0530

    Added intial code for authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,14 @@ class SessionsController < ApplicationController
   def create
     email = params[:email]
     password = params[:password]
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      redirect_to @user, notice: "Welcome Back #{@user.name}!"
+    else
+      flash.now[:alert] = 'Invalid email or password!'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
