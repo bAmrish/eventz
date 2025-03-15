860a8d5c0a4f26fb69e59c19c8efceb0f3fa2ae8
Amrish Bharatiya <bamrish@gmail.com>
2025-03-15 12:51:07 +0530

    Added code to prevent user controller actions to signed in users only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    User.find(session[:user_id]) if is_logged_in?
+  end
+
+  def is_logged_in?
+    session[:user_id] != nil
+  end
+
+  helper_method :current_user, :is_logged_in?
+
+  def require_signin
+    unless is_logged_in?
+      redirect_to login_path, notice: 'You need to be logged in!'
+    end
+  end
 end
