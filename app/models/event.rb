a43e061e8e539071cf3b5f367af9006e5be112c2
Amrish Bharatiya <bamrish@gmail.com>
2025-03-16 14:36:51 +0530

    Added code to validate uploaded image

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,6 +14,7 @@ class Event < ApplicationRecord
   validates :description, length: { minimum: 25 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :capacity, numericality: { only_integer: true, greater_than: 0 }
+  validate :acceptable_image
 
   scope :upcoming, -> {where("starts_at >= ?", Time.now).order("starts_at")}
   scope :past, -> {where("starts_at <= ?", Time.now).order("starts_at")}
@@ -31,8 +32,22 @@ class Event < ApplicationRecord
   def to_param
     slug
   end
+
 private
   def set_slug
     self.slug = name.parameterize
   end
+
+  def acceptable_image
+    return unless main_image.attached?
+    unless main_image.blob.byte_size <= 1.megabytes
+      errors.add(:main_image, "is too big. should be less than 1MB")
+    end
+
+    acceptable_types = ["image/png", "image/jpeg"]
+    unless acceptable_types.include?(main_image.content_type)
+      errors.add(:main_image, "is invalid type. Must be PNG or JPEG")
+    end
+  end
+
 end
